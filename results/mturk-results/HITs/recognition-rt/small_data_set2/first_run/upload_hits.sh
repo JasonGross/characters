364c8999f574840d38f4420dadc15ff0e1d67083
Jason Gross <jgross@mit.edu>
2011-04-02 01:34:12 -0400

    Updated task to suggest the 'a' key in more places, uploaded task to sandbox.

--- results/mturk-results/HITs/recognition-rt/small_data_set2/first_run/upload_hits.sh
+++ results/mturk-results/HITs/recognition-rt/small_data_set2/first_run/upload_hits.sh
@@ -3,11 +3,12 @@ source hit_header.sh "$@"
 
 echo Backing up files...
 pushd "$SCRIPTPATH" 1>/dev/null
-for file in $HITNAME.input.success*
+cp -f "$HITNAME.input" "$HITNAME$SANDBOX.input"
+for file in $HITNAME$SANDBOX.input.success*
 do
 	mv "$file" "${file}2.bak" 2>/dev/null
 done
-for file in upload-results.txt*
+for file in upload-results$SANDBOX.txt*
 do
 	mv "$file" "${file}2.bak" 2>/dev/null
 done
@@ -15,6 +16,6 @@ popd 1>/dev/null
 pushd "$MTURK_CMD_HOME/bin" 1>/dev/null
 # from http://fritzthomas.com/open-source/linux/384-how-to-get-the-absolute-path-within-the-running-bash-script/
 echo Uploading HIT in $SCRIPTPATH to turk $SANDBOX
-./loadHITs.sh -input "$SCRIPTPATH/$HITNAME.input" -question "$SCRIPTPATH/$HITNAME.question" -properties "$SCRIPTPATH/$HITNAME.properties" $SANDBOX >> "$SCRIPTPATH/upload-results.txt"
+./loadHITs.sh -input "$SCRIPTPATH/$HITNAME$SANDBOX.input" -question "$SCRIPTPATH/$HITNAME.question" -properties "$SCRIPTPATH/$HITNAME.properties" $SANDBOX >> "$SCRIPTPATH/upload-results$SANDBOX.txt"
 popd 1>/dev/null
 
